2d814e2b32c1cfa0dd67cdce6b672b0628f3f2e2
Rithvij <rithvij315@gmail.com>
2023-09-07 13:37:28 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card"
+import data from "./data"
 
-function App() {
+export default function App() {
+
+  const dataArr = data.map(card => 
+    <Card 
+      key={card.id}
+      {...card}
+    />)
+    
+  console.log(dataArr)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">
+        {dataArr}
+      </section>
     </div>
+  
   );
 }
 
-export default App;
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+
+
+export default function Hero() {
+    return (
+        <div className="hero">
+            <img src="./images/Group 77.png" className="hero--photogrid"></img>
+            <h1 className="hero--header">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    )
+}
